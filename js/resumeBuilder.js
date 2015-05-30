778d078d36909bef67cabb100ec6831d9eec432d
Siddhanth Maheshwari <siddhanth.maheshwari21@gmail.com>
2015-05-30 16:20:39 +0800

    Work in progress, Will come back to it later!

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,7 +7,7 @@ var education={
         },
         {
             "Name": "UIUC",
-            "Location": "Chicago",
+            "Location": "Chicago,USA",
             "majors": [
                 "CS"
             ],
@@ -37,7 +37,7 @@ var bio = {
 			"mobile"    : "217-607-3452",
 			"email"     : "siddhanthm@outlook.com",
 			"github"    : "SidMaheshwari",
-			"location"  : "Urbana, IL"
+			"location"  : "Urbana,IL,USA"
 		},
 	"skills" : ["Web_developer", "Can kill batman"]
 }
@@ -46,7 +46,13 @@ var projects = {
 	"projects" : [
 		{
 			"title" : "Sudoku Solver",
-			"dates" : "May 2017"
+			"dates" : "May 2017",
+			"description" : "Create a Sudoku solver which would taken a puzzle and give a solution to it if only one solution existed."
+		},
+		{
+			"title" : "HTML website",
+			"dates" : "Summer 2017",
+			"description" : "Replicated website given by designer"
 		}
 	]
 }
@@ -57,14 +63,14 @@ var work = {
 			"employer" : "UIUC",
 			"title"    : "Undergrad TA for CS 173",
 			"dates"	   : "Fall 2014 - Present",
-			"location" : "Urbana",
+			"location" : "Urbana,IL",
 			"description" : "Hold discussion sections and office hours"
 		},
 		{
 			"employer" : "UIUC",
 			"title"    : "Undergrad TA for ECE 110",
 			"dates"	   : "Fall 2014 - Present",
-			"location" : "Urbana",
+			"location" : "Urbana,IL",
 			"description" : "Hold office hours"
 		}
 	]
@@ -87,8 +93,43 @@ var displayWork = function() {
 		var title = HTMLworkTitle.replace("%data%", work.jobs[job].title);
 		var dates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
 		var description = HTMLworkDescription.replace("%data%",work.jobs[job].description);
+		var loc = HTMLworkLocation.replace("%data%", work.jobs[job].location);
 
-		$(".work-entry:last").append(employer + title + dates + description);
+		$(".work-entry:last").append(employer + title + dates + loc +  description);
 
 	}
 }
+
+
+
+
+$(document).click(function(loc) {
+
+	var x = loc.pageX;
+	var y = loc.pageY;
+	logClicks(x,y); 
+});
+
+function inName(name) {
+	name = name.trim.split(" ");
+	var firsthalf = name[0].slice(0,1);
+	var second = name[0].slice(1);
+	return firsthalf.toUpperCase() + second.toLowerCase() + " " + name[1].toUpperCase;
+}
+
+//$("#main").append(internationalizeButton);
+
+projects.display = function(){
+	for(var project in projects.projects){
+		$("#projects").append(HTMLprojectStart);
+
+		var title = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		var dates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		var description = HTMLprojectDescription.replace("%data%",projects.projects[project].description);
+		$(".project-entry:last").append(title + dates + description);
+	}
+}
+projects.display();
+displayWork();
+
+//$("#main").append(googleMap);
